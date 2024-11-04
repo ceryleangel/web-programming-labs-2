165ff412ad1617d00a08fc7622c48e1ef68a9e8e
Арина <babiiarina7@gmail.com>
2024-11-04 15:47:13 +0700

    Хэшировнаие

--- lab5.py
+++ lab5.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, session
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash, generate_password_hash
 
 lab5=Blueprint('lab5', __name__) 
 
@@ -48,14 +49,15 @@ def login():
         return render_template('lab5/login.html',
                                error = 'Логин и/или пароль неверны')
 
-    if user['password'] != password:
+    if not check_password_hash (user['password'], password):
             db_close(conn, cur)
-            return render_template ('lab2/login.html', 
+            return render_template ('lab5/login.html', 
                                     error='Логин и/или пароль неверны')
     
     session['login'] = login    
     db_close(conn, cur)
     return render_template('lab5/success_login.html', login=login)
+
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
@@ -76,8 +78,9 @@ def register():
         return render_template('lab5/register.html', 
                                error= 'Такой пользователь уже существует')
     
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
-    conn.commit()
+    password_hash = generate_password_hash(password)
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
