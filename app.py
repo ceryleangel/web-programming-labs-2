17de43c1518ad66aad54ff21c92f4fc52d99dae7
Арина <babiiarina7@gmail.com>
2024-12-17 19:52:13 +0700

    Лабораторная 7

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 
 import os
 
@@ -19,6 +20,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 @app.errorhandler(404)
 def not_found(err):
@@ -80,6 +82,7 @@ def index():
             <li><a href="/lab4">Четвертая Лабораторная</a></li>
             <li><a href="/lab5">Пятая Лабораторная</a></li>
             <li><a href="/lab6">Шестая Лабораторная</a></li>
+            <li><a href="/lab7">Седьмая Лабораторная</a></li>
         </ol>
     </body>
     <footer>
