4ee7364cb1fd292a2c13f21981f27856ae192f66
Арина <babiiarina7@gmail.com>
2024-12-17 20:39:14 +0700

    добавление фильма

--- lab7.py
+++ lab7.py
@@ -6,7 +6,7 @@ lab7 = Blueprint('lab7', __name__)
 
 @lab7.route('/lab7/')
 def main ():
-    return render_template('lab7/index.html')
+    return render_template('lab7/lab7.html')
 
 films = [
     {
